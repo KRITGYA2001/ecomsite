586dfa422f96a039e31cbabaf6ba44a45813878b
Kritgya2001 <kritgyakumar92@gmail.com>
2022-11-20 15:45:17 +0530

    fses

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     def __str__(self):
-        return self.title
+        return self.titleS
     
     title = models.CharField(max_length=200)
     price = models.FloatField()
